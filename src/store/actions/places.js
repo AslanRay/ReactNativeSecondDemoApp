084121c566c5d811c539c36906aaafb8ef300d47
AslanRay <aslan_ray@icloud.com>
2019-04-25 16:01:50 -0700

    Integracion de firebase y db

--- src/store/actions/places.js
+++ src/store/actions/places.js
@@ -8,7 +8,55 @@ export const addPlace = (placeName,region, image) => {
         region: region,
         image:image
     };
+
+    // -- SUBIR DATOS A LA DB -- //
+    // return dispatch => {
+    //     const placeData = {
+    //         name: placeName,
+    //         location: region
+    //     }
+    //     fetch('https://basicdemoapp-f38fb.firebaseio.com/places.json',{
+    //         method:'POST',
+    //         body: JSON.stringify(placeData)
+    //     }).catch(error => console.log(error))
+    //     .then(response => response.json())
+    //     .then(parsedResponse => {
+    //         console.log(parsedResponse);
+    //     });
+    // }
 };
+// -- DESCARGAR DATOS DE LA DB -- //
+// export const getPlaces = () => {
+//     return dispatch => {
+//         fetch("https://basicdemoapp-f38fb.firebaseio.com/places.json")
+//         .catch(err => {
+//             alert("Algo salio mal");
+//             console.log(err);
+//         })
+//         .then(res => res.json())
+//         .then(parsedRes => {
+//             const places = [];
+//             for (let key in parsedRes) {
+//                 places.push({
+//                     ...parsedRes[key],
+//                     image: {
+//                         uri: parsedRes[key].image
+//                     },
+//                     key: key
+//                 });
+//             }
+//             dispatch(setPlaces(places));
+//         });
+//     };
+// };
+
+// export const setPlaces = places => {
+//     return {
+//         type: SET_PLACES,
+//         places: places
+//     };
+// };
+
 
 export const deletePlace = (key) => {
     return {
